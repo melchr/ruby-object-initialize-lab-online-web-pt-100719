da22065db417afbb3f310f8e972a67d5aed2eefc
Melissa Christman <melchrstmn@gmail.com>
2019-10-18 03:14:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,5 @@
 class Dog
-def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
-  end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
-  def initialize(breed = "Mutt")
     @breed = breed
   end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
-end
